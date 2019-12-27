c56c678be98100848336eaf001bbcc3e875e401c
ectuser <m.ectuser@gmail.com>
2019-12-27 17:46:19 +0700

    If conditions fix

--- Interpreter.py
+++ Interpreter.py
@@ -24,8 +24,9 @@ class Interpreter:
 			print("Start interpreter")
 
 		for command in self.commands:
-			self.handle_if(command)
+			self.handle_end(command)
 			if len(self.condition_stack) == 0 or self.condition_stack[len(self.condition_stack) - 1]:
+				self.handle_if(command)
 				self.handle_declaration(command)
 				self.handle_assigment(command)
 				self.handle_print(command)
@@ -139,7 +140,7 @@ class Interpreter:
 
 			first_exp = command[2:operator_pos]
 			second_exp = command[operator_pos + 1:len(command) - 3]
-
+			
 			first_res = self.count_expression(first_exp)
 			second_res = self.count_expression(second_exp)
 			res = self.compare(first_res, second_res, command[operator_pos].content)
@@ -147,6 +148,8 @@ class Interpreter:
 				self.condition_stack.append(True)
 			else:
 				self.condition_stack.append(False)
+	
+	def handle_end(self, command):
 		if (command[0].type == 'END'):
 			self.condition_stack.pop()
 				
